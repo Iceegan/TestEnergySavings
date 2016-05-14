8318034eed6fd9de2608d7324d77df437a33d2d9
Iceegan <keegancampanelli@gmail.com>
2016-05-14 15:48:03 -0400

    Completed the SettingsActivity but still need to add text for the about Dialog

--- app/src/main/java/org/magna/energysavings/SettingsActivity.java
+++ app/src/main/java/org/magna/energysavings/SettingsActivity.java
@@ -1,13 +1,56 @@
 package org.magna.energysavings;
 
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class SettingsActivity extends Activity {
+    private AlertDialog mDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        //declarations
+        final TextView aboutText = (TextView) findViewById(R.id.about_tv);
+        final TextView helpText = (TextView) findViewById(R.id.help_tv);
+
+        aboutText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(SettingsActivity.this);
+                builder.setTitle(R.string.about_title);
+                builder.setMessage(R.string.about_text);
+                builder.setCancelable(false);
+                builder.setPositiveButton(R.string.ok_label, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //nothing
+                    }
+                });
+                mDialog = builder.show();
+            }
+        });
+        helpText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(SettingsActivity.this);
+                builder.setTitle(R.string.help_title);
+                builder.setMessage(R.string.help_text);
+                builder.setCancelable(false);
+                builder.setPositiveButton(R.string.ok_label, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //nothing
+                    }
+                });
+                mDialog = builder.show();
+            }
+        });
     }
 }

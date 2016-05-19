4b550e17b1c93b8f9f92e5338a16663085657823
Iceegan <keegancampanelli@gmail.com>
2016-05-18 20:51:40 -0400

    Finishing touches, all that is left is the back buttons

--- app/src/main/java/org/magna/energysavings/SettingsActivity.java
+++ app/src/main/java/org/magna/energysavings/SettingsActivity.java
@@ -15,6 +15,8 @@ public class SettingsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        getActionBar().setHomeButtonEnabled(true);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
 
         //declarations
         final TextView aboutText = (TextView) findViewById(R.id.about_tv);
--- app/src/main/java/org/magna/energysavings/TipsActivity.java
+++ app/src/main/java/org/magna/energysavings/TipsActivity.java
@@ -56,5 +56,15 @@ public class TipsActivity extends Activity {
             }
         });
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
 
